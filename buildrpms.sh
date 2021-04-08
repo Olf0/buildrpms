138e0032f561f48a547b598f56180d60463873ce
olf <Olf0@users.noreply.github.com>
2021-04-08 20:41:48 +0200

    Update buildrpms.sh

--- buildrpms.sh
+++ buildrpms.sh
@@ -81,7 +81,7 @@ do
       then echo "- Warning: Ignoring ${j}, because SOURCES/$Archive already exists." | tee -a "$Logfile"
       else
         echo "- $j" | tee -a "$Logfile"
-        mkdir -p SOURCES
+        mkdir -p SOURCES  # ToDo: Stop creating any directory, instead test for their existance at the start!
         mv "$j" SOURCES/
         Moved="${Moved},$(echo "$Archive" | sed 's/\.tar.*$//')"
       fi
